9b03cf9872b02ab1c94f53f5f4cac360415728ca
aadeyemi <aadeyemi@chams.com>
2020-01-29 17:31:49 +0100

    Changes on DbContext

---
+++ LiveProduction-Api.Core/InterfaceServices/IUserServices.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiveProduction_Api.Core.InterfaceServices
+{
+    public interface IUserServices
+    {
+
+    }
+}
